ad06480589a30a2f306b3a03e2116ee81c60180d
Jian Jang <pureunnabi@gmail.com>
2023-09-16 22:31:24 +0900

    규칙 불러오기

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -13,22 +13,19 @@ router.post('/check-duplicate', userController.checkDuplicate);
 router.post('/login', userController.login);
 
 // 로그아웃 라우트 (DELETE)
-router.delete('/logout', userController.logout);
+router.post('/logout', userController.logout);
 
 // 탈퇴 라우트 (DELETE)
-router.delete('/leave', getUser, userController.leave);
+router.post('/leave', getUser, userController.leave);
 
 // 프로필 설정 라우트
-router.post("/setProfile", getUser,userController.setProfile);
+router.post("/setProfile", getUser, userController.setProfile);
 
 // 프로필 수정 라우트
-router.post('/update-profile', getUser,userController.updateProfile);
+router.post('/update-profile', getUser, userController.updateProfile);
 
 // 프로필 가져오는 라우트
-router.get("/getUserInfo", getUser,userController.getUserInfo);
-
-// 프로필 상태 라우트
-// router.get('/hasProfile', getUser, userController.hasProfile);
+router.get("/getUserInfo", getUser, userController.getUserInfo);
 
 // 사용자 ID 변경 라우트
 router.post('/change-user-id', getUser, userController.changeUserId);
@@ -36,8 +33,17 @@ router.post('/change-user-id', getUser, userController.changeUserId);
 // 사용자 PW 변경 라우트
 router.post('/change-password', getUser, userController.changePassword);
 
+// 아이디 찾기 라우트
+router.post('/findUserId', userController.findUserId);
+
 // 규칙 추가 라우트 
 router.post("/rules", userController.rules);
+
+// 규칙 불러오기 라우트 
+router.get("/getRules",getUser, userController.getRules);
+
+// 규칙 추가 라우트 
+router.get("/getColor",getUser, userController.getCalendarColor);
 
 // 카운트 증가 추가 라우트 
 router.post("/increaseCount", userController.increaseCount);
@@ -50,6 +56,7 @@ router.post("/certificate", userController.certificate);
 
 // 인증번호 확인 라우트
 router.post("/check-auth-code", userController.checkAuthCode);
+
 
 
 module.exports = router;
