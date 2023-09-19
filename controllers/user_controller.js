441bf8c082c4601fcc2a9f44b417f63c0b3643a3
Jian Jang <pureunnabi@gmail.com>
2023-09-19 14:43:54 +0900

    문자열로 바꾸기

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -32,6 +32,16 @@ exports.signup = (req, res) => {
   const { userid, pw, email } = req.body;
   const currentDate = new Date();
 
+  const year = currentDate.getFullYear();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // 월은 0부터 시작하므로 +1 필요
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const hours = currentDate.getHours().toString().padStart(2, '0');
+  const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+  const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  console.log(formattedDate);
+
   const sql = 'INSERT INTO users (userid, pw, email, registration_date) VALUES (?, ?, ?, ?)';
   connection.query(sql, [userid, pw, email, currentDate], (err, result) => {
     if (err) {
@@ -397,6 +407,16 @@ exports.updateCounts = (req, res) => {
 exports.calendar = (req, res) => {
   const { userid } = req.body;
   const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // 월은 0부터 시작하므로 +1 필요
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const hours = currentDate.getHours().toString().padStart(2, '0');
+  const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+  const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  console.log(formattedDate);
   const sql = 'INSERT INTO calendar_date (userid, completedate) VALUES (?, ?)';
   connection.query(sql, [userid, currentDate], (err, result) => {
     if (err) {
