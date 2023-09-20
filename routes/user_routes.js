4756dcf73976e81412ff644679ec562918500ed8
Jian Jang <pureunnabi@gmail.com>
2023-09-20 18:08:38 +0900

    정리 2

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user_controller'); // 사용자 컨트롤러 가져오기
-const { upload } = require("../controllers/upload_img");
 const { getUser } = require('../modules/getUser');
   
 
